e7e1d1560132785c5bb74058ce5ee2a290451de2
Kishen S Jain <jain.skishen@gmail.com>
2026-03-10 11:49:37 +0530

    Create user model

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from .database import engine
+from . import models
 
-app = FastAPI()
+models.Base.metadata.create_all(bind=engine) #It tells SQLAlchemy: Create tables in the database if they do not exist.
+
+app = FastAPI(title="FastAPI CRUD Auth")
 
 @app.get("/")
 def root():
