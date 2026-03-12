dc7bc930c1d66347a3eb7adaf7bc6347874e410f
Kishen S Jain <jain.skishen@gmail.com>
2026-03-12 10:21:05 +0530

    Add authentication using bcrypt

---
+++ src/auth.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timedelta, timezone
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES") or 30)
+
+def hash_password(password : str):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password : str, hashed_password : str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data:dict):
+    to_encode = data.copy()
+
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return encoded_jwt
